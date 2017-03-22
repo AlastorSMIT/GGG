d7b3c71aa6f20d93c8b5da0a7fc03b488fabe2da
AlexShpil <alexshpil@gmail.com>
2017-03-22 09:47:46 +0200

    Update main.py
    Wrapper accepts target dirrectory or file and adress to copy as input

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-import version
-from version import version_python
-print (version_python().major)
+#!/usr/bin/python
+import os
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("target", type=str,
+                    help="path to dir or file")
+parser.add_argument("adress_to_copy", type=str,
+                    help="remote adress which will store copy")
+parser.add_argument("-v", "--verbose", action="store_true",
+                    help="increase output verbosity")
+args = parser.parse_args()
+if args.verbose:
+    print("Copy local {} to remote machine with adress {}".format(args.target, args.adress_to_copy))
+else:
+    print(args.target, args.adress)
+os.system("rsync {0} {1}".format(args.target, args.adress_to_copy))
